745c1da5559e5e42ba8d0589419c59d81096a8a3
IlyaOsipenkov <dakarlog@gmail.com>
2023-01-11 06:20:48 +0300

    Todo-Practice

---
+++ src/Components/SearchPanel.js
@@ -0,0 +1,9 @@
+const SearchPanel = () => {
+    const searchStyles = {
+        fontSize: "25px",
+    };
+
+    return <input style={searchStyles} placeholder="search" />;
+};
+
+export default SearchPanel;
---
+++ src/Components/TodoList.js
@@ -0,0 +1,15 @@
+import TodoListItem from "./TodoLostItem";
+
+const TodoList = ({ todos }) => {
+    const elements = todos.map((item) => {
+        return (
+            <li>
+                <TodoListItem {...item} />
+            </li>
+        );
+    });
+
+    return <ul>{elements}</ul>;
+};
+
+export default TodoList;
